72ea01f507ab18bf1bb96e9a5573a57f54621433
Guillaume Deroire <guillaume.deroire@hach.com>
2020-02-19 14:49:15 +0100

    SC4200 R2019.4

---
+++ clean_OS_intermediates.sh
@@ -0,0 +1,12 @@
+# From .repo/manifests, go back to base folder fusion_seacloud_platform
+pushd ../.. > /dev/null
+
+# Export environment variables
+SOURCE_OUTPUT=${PWD}/source_output
+source ./export &> ${SOURCE_OUTPUT} || cat ${SOURCE_OUTPUT}
+rm ${SOURCE_OUTPUT}; unset SOURCE_OUTPUT
+
+# Launch bitbake to clean intermediate artifacts
+time bitbake -k -f cleanall world 2>&1
+
+popd > /dev/null
